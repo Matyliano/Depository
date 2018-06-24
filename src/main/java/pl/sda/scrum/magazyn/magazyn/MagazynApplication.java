7b9debf64add6c8c7a758c286a8d3b882ebbe59b
Jarek_k <kolasa.jarek@gmail.com>
2018-06-24 10:44:53 +0200

    First commit

---
+++ src/main/java/pl/sda/scrum/magazyn/magazyn/MagazynApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.scrum.magazyn.magazyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazynApplication.class, args);
+    }
+}
