af6f651b02aeaf0d2d81b1bfa1f59b8bb294ba06
Jarek_k <kolasa.jarek@gmail.com>
2018-06-24 12:10:52 +0200

    model controller updated

--- src/main/java/pl/sda/scrum/magazyn/magazyn/controllers/DepositoryListController.java
+++ src/main/java/pl/sda/scrum/magazyn/magazyn/controllers/DepositoryListController.java
@@ -1,12 +1,22 @@
 package pl.sda.scrum.magazyn.magazyn.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.scrum.magazyn.magazyn.model.DepositoryList;
+import pl.sda.scrum.magazyn.magazyn.model.DepositoryModel;
+import pl.sda.scrum.magazyn.magazyn.repository.DepositoryListRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 @CrossOrigin()
 public class DepositoryListController {
+
+    @Autowired
+    DepositoryListRepository depositoryListRepository;
 
 }
